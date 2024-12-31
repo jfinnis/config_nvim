1eadd8636fafeb836361bcea5a143cec0d34aa93
Josh Finnis <josh.finnis@bunchball.com>
2024-12-30 23:39:58 -0500

    update color scheme to add transparency

--- lua/config/plugins2/colorschemes.lua
+++ lua/config/plugins2/colorschemes.lua
@@ -11,16 +11,29 @@ return {
                 bold_vert_split = true,
                 -- transparent telescope.nvim -- TODO doesn't do anything?
                 highlight_groups = {
-                    TelescopeBorder = { fg = "highlight_high", bg = "none" },
-                    TelescopeNormal = { bg = "none" },
-                    TelescopePromptNormal = { bg = "base" },
-                    TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-                    TelescopeSelection = { fg = "text", bg = "base" },
-                    TelescopeSelectionCaret = { fg = "rose", bg = "none" },
+                    TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+                    TelescopeNormal = { bg = 'none' },
+                    TelescopePromptNormal = { bg = 'base' },
+                    TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+                    TelescopeSelection = { fg = 'text', bg = 'base' },
+                    TelescopeSelectionCaret = { fg = 'rose', bg = 'none' },
                 }
             })
             --vim.cmd('colorscheme rose-pine')
         end
     },
-    { 'EdenEast/nightfox.nvim' },
+
+    {
+        'EdenEast/nightfox.nvim',
+        config = function()
+            require('nightfox').setup {
+                options = {
+                    transparent = true,
+                },
+                styles = {
+                    comments = 'italics',
+                }
+            }
+        end
+    },
 }
