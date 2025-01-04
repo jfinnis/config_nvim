749813da86693e7b6b9e90ef84645c64e8151e9d
Josh Finnis <josh.finnis@bunchball.com>
2025-01-04 11:08:43 -0500

    go through blink config options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,31 +46,6 @@ require("lazy").setup({
             opts = {},
         },
 
-        -- Autocompletion
-        -- {
-        --     'hrsh7th/nvim-cmp',
-        --     event = 'InsertEnter',
-        --     config = function()
-        --         local cmp = require('cmp')
-        --
-        --         cmp.setup({
-        --             sources = {
-        --                 {name = 'nvim_lsp'},
-        --             },
-        --             mapping = cmp.mapping.preset.insert({
-        --                 ['<C-Space>'] = cmp.mapping.complete(),
-        --                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        --                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        --             }),
-        --             snippet = {
-        --                 expand = function(args)
-        --                     vim.snippet.expand(args.body)
-        --                 end,
-        --             },
-        --         })
-        --     end
-        -- },
-
         -- pretty_hover - clean up lsp dialogs
         -- {
         --     'Fildo7525/pretty_hover',
--- lua/config/plugins2/completion.lua
+++ lua/config/plugins2/completion.lua
@@ -8,9 +8,12 @@ return {
         dependencies = 'rafamadriz/friendly-snippets',
         version = '*',
         opts = {
-            keymap = { preset = 'default' },
+            keymap = {
+                preset = 'default',
+                -- scroll_documentation_up and down - how to do?
+                -- snippet_forward backward - how to do snippets
+            },
             appearance = {
-                use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'mono'
             },
             sources = {
@@ -25,8 +28,81 @@ return {
                     },
                 },
             },
-            signature = { enabled = true }, -- in teej config?
+            -- still experimental
+            signature = {
+                enabled = true,
+                window = {
+                    border = 'rounded'
+                }
+            },
+
+            completion = {
+                -- experiment with, will i find this annoying to always popup?
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                    window = {
+                        border = 'rounded',
+                    }
+                },
+                menu = {
+                    draw = {
+                        -- experimental
+                        treesitter = { 'lsp' },
+                        -- all of the components table is from colorful-menu plugin
+                        components = {
+                            label = {
+                                width = { fill = true, max = 60 },
+                                text = function(ctx)
+                                    local highlights_info =
+                                    require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+                                    if highlights_info ~= nil then
+                                        return highlights_info.text
+                                    else
+                                        return ctx.label
+                                    end
+                                end,
+                                highlight = function(ctx)
+                                    local highlights_info =
+                                    require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+                                    local highlights = {}
+                                    if highlights_info ~= nil then
+                                        for _, info in ipairs(highlights_info.highlights) do
+                                            table.insert(highlights, {
+                                                info.range[1],
+                                                info.range[2],
+                                                group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
+                                            })
+                                        end
+                                    end
+                                    for _, idx in ipairs(ctx.label_matched_indices) do
+                                        table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                                    end
+                                    return highlights
+                                end,
+                            },
+                        },
+                    },
+                    -- don't show completion menu when searching
+                    auto_show = function(ctx)
+                        return ctx.mode ~= 'cmdline' or not vim.tbl_contains({ '/', '?' }, vim.fn.getcmdtype())
+                    end
+                },
+            },
         },
-        opts_extend = { "sources.default" }
+        opts_extend = { 'sources.default' }
+    },
+    {
+        'xzbdmw/colorful-menu.nvim',
+        config = function()
+            -- You don't need to set these options.
+            require('colorful-menu').setup({
+                ft = {
+                    typescript = {
+                        enabled = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript', 'javascriptreact' },
+                    }
+                }
+            })
+        end,
     }
 }
