1f9aaaf3ee63716ff077e6309cd515e67d29a9f6
Josh Finnis <josh.finnis@bunchball.com>
2025-01-09 21:35:24 -0500

    add illuminate plugin

---
+++ lua/config/plugins/illuminate.lua
@@ -0,0 +1,9 @@
+return {
+    'rrethy/vim-illuminate',
+    event = 'VeryLazy',
+    config = function()
+        require('illuminate').configure {
+            under_cursor = false,
+        }
+    end,
+}
