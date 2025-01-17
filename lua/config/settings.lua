30a80bce9e65657e39bb5c1c044b98a73d72f7d3
Josh Finnis <josh.finnis@bunchball.com>
2025-01-17 18:57:09 -0500

    restore cursor to file position in previous session

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -33,9 +33,20 @@ vim.api.nvim_create_autocmd(
     { pattern = '*', command = 'set nocursorline nocursorcolumn', group = cursorGroup }
 )
 
---- auto resize splits when the terminal's window is resized
+--- Auto resize splits when the terminal's window is resized
 vim.api.nvim_create_autocmd('VimResized', {
     command = 'wincmd =',
+})
+
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd('BufReadPost', {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
 })
 
 -- highlight yanked text briefly - replaced with Tiny Glimmer plugin for animated yank
