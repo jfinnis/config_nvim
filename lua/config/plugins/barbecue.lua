a120eaba2a79fa2e33316ebab58be991b6015f99
Josh Finnis <josh.finnis@bunchball.com>
2025-01-18 00:05:30 -0500

    add barbecue breadcrumbs plugin

---
+++ lua/config/plugins/barbecue.lua
@@ -0,0 +1,16 @@
+--
+-- barbecue - breadcrumbs as the top line
+--
+
+return {
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+        'SmiteshP/nvim-navic',
+        'nvim-tree/nvim-web-devicons', -- optional dependency
+    },
+    opts = {
+        -- configurations go here
+    },
+}
