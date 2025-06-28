025cff6fe54ccb695b05319779c8a79b399d6528
Joshua Finnis <joshfinnis@gmail.com>
2025-06-28 09:54:55 -0400

    update fidget settings

--- lua/config/plugins/fidget.lua
+++ lua/config/plugins/fidget.lua
@@ -9,15 +9,25 @@ return {
     'j-hui/fidget.nvim',
     opts = {
         notification = {
+            filter = vim.log.levels.INFO,
             override_vim_notify = true,  -- Automatically override vim.notify() with Fidget
             view = {
-                stack_upwards = false, -- Display notification items from bottom to top
+                stack_upwards = true, -- Display notification items from bottom to top
             },
             window = {
                 border = 'rounded',
-                winblend = 0,
-                align = 'top',
+                winblend = 20,
+                align = 'bottom',
+                x_padding = 0,
             },
         },
+
+        -- lsp subsystem
+        progress = {
+            ignore = {}, -- list of lsp servers to ignore
+            display = {
+                skip_history = true, -- progress notifications omitted from history
+            }
+        }
     },
 }
